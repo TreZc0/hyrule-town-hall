aaf99399adb296d73e4562a78abb774bf6c53797
TreZc0_ <trezc0@gmx.de>
2026-02-03 23:01:55 +0100

    better twwr permalink display

--- src/seed.rs
+++ src/seed.rs
@@ -276,15 +276,8 @@ pub(crate) async fn table_cell(now: DateTime<Utc>, seed: &Data, spoiler_logs: bo
             a(href = format!("https://www.triforceblitz.com/seed/daily/{ordinal}"), target = "_blank") : "View";
         }),
         Some(Files::TwwrPermalink { ref permalink, ref seed_hash }) => Some(html! {
-            div(class = "twwr-seed") {
-                div {
-                    strong : "Permalink: ";
-                    code(style = "user-select: all") : permalink;
-                }
-                div {
-                    strong : "Seed Hash: ";
-                    : seed_hash;
-                }
+            span(class = "settings-link twwr-seed-link", data_tooltip = format!("Permalink: {}\nSeed Hash: {}", permalink, seed_hash)) {
+                : "Hover for Seed";
             }
         }),
         None => None,
