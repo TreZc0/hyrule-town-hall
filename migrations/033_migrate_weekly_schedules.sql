3254f46c63522b581069f796d68309e9fd5c0ed0
TreZc0_ <trezc0@gmx.de>
2026-02-18 22:53:00 +0100

    fix migration

--- migrations/033_migrate_weekly_schedules.sql
+++ migrations/033_migrate_weekly_schedules.sql
@@ -4,12 +4,6 @@
 -- settings_description is "variety" (from SHORT_WEEKLY_SETTINGS constant)
 -- notification_channel_id is NULL (removing the @Standard role ping)
 -- room_open_minutes_before is 30 (current default)
-INSERT INTO weekly_schedules (id, series, event, name, frequency_days, time_of_day, timezone, anchor_date, active, settings_description, notification_channel_id, room_open_minutes_before) VALUES
-(1, 's', 'w', 'Kokiri', 14, '18:00:00', 'America/New_York', '2025-01-04', false, 'variety', NULL, 30),
-(2, 's', 'w', 'Goron', 14, '14:00:00', 'America/New_York', '2025-01-05', false, 'variety', NULL, 30),
-(3, 's', 'w', 'Zora', 14, '14:00:00', 'America/New_York', '2025-01-11', false, 'variety', NULL, 30),
-(4, 's', 'w', 'Gerudo', 14, '09:00:00', 'America/New_York', '2025-01-12', false, 'variety', NULL, 30);
-
 -- TWWR weekly (active)
 INSERT INTO weekly_schedules (id, series, event, name, frequency_days, time_of_day, timezone, anchor_date, active, settings_description, notification_channel_id, room_open_minutes_before) VALUES
 (5, 'twwrmain', 'w', 'Saturday', 7, '18:00:00', 'America/New_York', '2026-01-24', true, 'Preliminary Miniblins S2 Settings', NULL, 30);
