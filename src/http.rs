a9b886ad8c0a9a30c97ce1b9c8206588250dec9f
TreZc0_ <trezc0@gmx.de>
2026-03-15 18:18:23 +0100

    feat: api qual standings, cleaner optout

--- src/http.rs
+++ src/http.rs
@@ -581,6 +581,7 @@ pub(crate) async fn rocket(pool: PgPool, discord_ctx: RwFuture<DiscordCtx>, http
         api::graphql_query,
         api::graphql_playground,
         api::entrants_csv,
+        api::qualifier_standings,
         api::swiss_standings_endpoint,
         api::racetime_goals,
         auth::racetime_callback,
