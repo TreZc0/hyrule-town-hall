f8254471f64ce2009839a9f244b77cba4089bd2b
TreZc0 <trezc0@gmx.de>
2026-04-05 14:06:37 +0200

    merge

--- migrations/062_seed_gen_type_tfb_rsl.sql
+++
@@ -1,7 +0,0 @@
--- Populate seed_gen_type for Triforce Blitz events (OotrTriforceBlitz)
-UPDATE events SET seed_gen_type = 'ootr_tfb'
-WHERE racetime_goal_slug = 'triforce_blitz' AND seed_gen_type IS NULL;
-
--- Populate seed_gen_type for RSL and concluded Pictionary RSL events (OotrRsl)
-UPDATE events SET seed_gen_type = 'ootr_rsl'
-WHERE racetime_goal_slug IN ('rsl', 'pic_rs2') AND seed_gen_type IS NULL;
