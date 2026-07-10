89912e26f2c9f57a9d8dae51ebb7729e62270e6d
TreZc0 <trezc0@gmx.de>
2026-07-10 23:32:08 +0200

    fix: readd tooltip random settings info

--- src/cal.rs
+++ src/cal.rs
@@ -3046,7 +3046,8 @@ pub(crate) async fn race_table(
                                 @if race.show_seed() || race.is_ended() || matches!(race.schedule, RaceSchedule::Unscheduled | RaceSchedule::Async { .. } | RaceSchedule::Live { .. }) {
                                     @if let Some(racetime_bot::Goal::Crosskeys2025 | racetime_bot::Goal::Crosskeys2026) = racetime_bot::Goal::for_event(race.series, &race.event) {
                                         @if let Ok(crosskeys_options) = racetime_bot::CrosskeysRaceOptions::for_race_with_transaction(&mut *transaction, race).await {
-                                            span(class = "settings-link", data_tooltip = format!("Seed Settings: {}\nRace Rules: {}", crosskeys_options.as_seed_options_str(&[]), crosskeys_options.as_race_options_str_no_delay())) {
+                                            @let random_settings_str = crosskeys_options.random_seed_options_str(&[]).map(|s| format!(" (Random: {s})")).unwrap_or_default();
+                                            span(class = "settings-link", data_tooltip = format!("Seed Settings: {}{}\nRace Rules: {}", crosskeys_options.as_seed_options_str(&[]), random_settings_str, crosskeys_options.as_race_options_str_no_delay())) {
                                                 : " -Hover for Settings- ";
                                             }
                                         }
@@ -3058,7 +3059,8 @@ pub(crate) async fn race_table(
                             td {
                                 @if let Some(racetime_bot::Goal::Crosskeys2025 | racetime_bot::Goal::Crosskeys2026) = racetime_bot::Goal::for_event(race.series, &race.event) {
                                     @if let Ok(crosskeys_options) = racetime_bot::CrosskeysRaceOptions::for_race_with_transaction(&mut *transaction, race).await {
-                                        span(class = "settings-link", data_tooltip = format!("Seed Settings: {}\nRace Rules: {}", crosskeys_options.as_seed_options_str(&[]), crosskeys_options.as_race_options_str_no_delay())) {
+                                        @let random_settings_str = crosskeys_options.random_seed_options_str(&[]).map(|s| format!(" (Random: {s})")).unwrap_or_default();
+                                        span(class = "settings-link", data_tooltip = format!("Seed Settings: {}{}\nRace Rules: {}", crosskeys_options.as_seed_options_str(&[]), random_settings_str, crosskeys_options.as_race_options_str_no_delay())) {
                                             : "-Hover for Settings-";
                                         }
                                     }
